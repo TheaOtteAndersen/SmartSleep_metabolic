d0039f8c83f8897ae842f97f622c2edd29becea1
CSejling <99794797+CSejling@users.noreply.github.com>
2022-02-21 09:08:56 +0100

    Justering i forhold til nye grunddata

--- Analysis.R
+++ Analysis.R
@@ -1,29 +1,58 @@
 ###### Analysis of associations between metabolic biomarkers and smartphone activity ######
 
+#IMPORTANT: DATA WILL CONTAIN WEIGHTS AND IMP NUMBERS FROM THE BEGINNING. SO THERE WILL BE NO NEED TO COLLECT THESE. HENCE, DELETE THE LINES THAT ADD THESE TO THE DATA.
+
+
 #This script contains analyses of the multiply imputed data sets from the SmartSleep project.
 #The multiple imputation results are combined using Rubin's rules.
 
 library(dplyr)
 library(matrixStats)
 library(mice)
+library(MASS)
 
 #Reading in the data
-setwd("S:/SUND-IFSV-SmartSleep/Data cleaning/Data imputation/Data/Renset imputation")
-load("H:/SmartSleep backup IT issues/imputation/myImputationCSS-res-00001.RData")
-CSS0 <- imp_CSS$data
-CSS0$impnr=0
-CSS <- read.csv2("Citizen Science Sample/imp_citizenScience.csv")
-CSS <- rbind(CSS0,CSS)
-
-#CSS <- subset(CSS,impnr!=0)
+#setwd("S:/SUND-IFSV-SmartSleep/Data cleaning/Data imputation/Data/Renset imputation")
+#load("H:/SmartSleep backup IT issues/imputation/myImputationCSS-res-00001.RData")
+#CSS0 <- imp_CSS$data
+#CSS0$impnr=0
+#CSS <- read.csv2("Citizen Science Sample/imp_citizenScience.csv")
+#CSS <- rbind(CSS0,CSS)
 
 subject_tracking_clusters <- read.csv2("H:/SmartSleep backup IT issues/subject_tracking_clusters.csv") #Change to S-drive location
 
-base_weights <- read.csv2("S:/SUND-IFSV-SmartSleep/Thea/Clusters, obesity and metabolic biomarkers/Data/Citizen Science Sample/SmartSleepExpWeighted.csv")
+#base_weights <- read.csv2("S:/SUND-IFSV-SmartSleep/Thea/Clusters, obesity and metabolic biomarkers/Data/Citizen Science Sample/SmartSleepExpWeighted.csv")
 #load("S:/SUND-IFSV-SmartSleep/Data cleaning/Data imputation/Data/SmartSleep Experiment/full_imp_base.RData")
 #base_data <- full_imp_Base
+#base_data <- rename(read.csv2("S:/SUND-IFSV-SmartSleep/Data cleaning/Data imputation/Data/Renset imputation/Experiment/imp_Experiment.csv"),imputation=imp_nr)
+#base_data$zipCode<-as.numeric(base_data$zipCode)
+
 base_data <- rename(read.csv2("S:/SUND-IFSV-SmartSleep/Data cleaning/Data imputation/Data/Renset imputation/Experiment/imp_Experiment.csv"),imputation=imp_nr)
-base_data$zipCode<-as.numeric(base_data$zipCode)
+
+#Baseline data with self-reports
+
+base_data$selfScore <- (base_data$mobileUseBeforeSleep=="5-7 times per week")*4+(base_data$mobileUseBeforeSleep=="2-4 times per week")*3+(base_data$mobileUseBeforeSleep=="Once a week")*3+(base_data$mobileUseBeforeSleep=="Every month or less")*2+(base_data$mobileUseBeforeSleep=="Never")*1+
+  (base_data$mobileUseNight=="Every night or almost every night")*4+(base_data$mobileUseNight=="A few times a week")*3+(base_data$mobileUseNight=="A few times a month or less")*2+(base_data$mobileUseNight=="Never")*1+
+  (base_data$mobileCheck==">20 times an hour")*4+(base_data$mobileCheck=="11-20 times an hour")*4+(base_data$mobileCheck=="5-10 times an hour")*3+(base_data$mobileCheck=="1-4 times an hour")*2+(base_data$mobileCheck=="Every 2nd hour")*2+(base_data$mobileCheck=="Several times a day")*1+(base_data$mobileCheck=="Once a day or less")*1+
+  (base_data$pmpuScale<=14)*1+(base_data$pmpuScale>14 & base_data$pmpuScale<=16.76)*2+(base_data$pmpuScale>16.76 & base_data$pmpuScale<=19)*3+(base_data$pmpuScale>19)*4
+base_data$selfScoreCat <- "1"
+base_data$selfScoreCat[base_data$selfScore>=8]="2"
+base_data$selfScoreCat[base_data$selfScore>=9.517]="3"
+base_data$selfScoreCat[base_data$selfScore>=12]="4"
+
+
+#Followup sample
+CSS <- read.csv2("S:/SUND-IFSV-SmartSleep/Data cleaning/Data imputation/Data/Renset imputation/Citizen Science Sample/imp_citizenScience.csv")
+CSS_track <- inner_join(CSS,subject_tracking_clusters,by="userid")
+
+CSS_track$selfScore <- (CSS_track$mobileUseBeforeSleep=="5-7 times per week")*4+(CSS_track$mobileUseBeforeSleep=="2-4 times per week")*3+(CSS_track$mobileUseBeforeSleep=="Once a week")*3+(CSS_track$mobileUseBeforeSleep=="Every month or less")*2+(CSS_track$mobileUseBeforeSleep=="Never")*1+
+  (CSS_track$mobileUseNight=="Every night or almost every night")*4+(CSS_track$mobileUseNight=="A few times a week")*3+(CSS_track$mobileUseNight=="A few times a month or less")*2+(CSS_track$mobileUseNight=="Never")*1+
+  (CSS_track$mobileCheck==">20 times an hour")*4+(CSS_track$mobileCheck=="11-20 times an hour")*4+(CSS_track$mobileCheck=="5-10 times an hour")*3+(CSS_track$mobileCheck=="1-4 times an hour")*2+(CSS_track$mobileCheck=="Every 2nd hour")*2+(CSS_track$mobileCheck=="Several times a day")*1+(CSS_track$mobileCheck=="Once a day or less")*1+
+  (CSS_track$pmpuScale<=14)*1+(CSS_track$pmpuScale>14 & CSS_track$pmpuScale<=16.76)*2+(CSS_track$pmpuScale>16.76 & CSS_track$pmpuScale<=19)*3+(CSS_track$pmpuScale>19)*4
+CSS_track$selfScoreCat <- "1"
+CSS_track$selfScoreCat[CSS_track$selfScore>=8]="2"
+CSS_track$selfScoreCat[CSS_track$selfScore>=9.517]="3"
+CSS_track$selfScoreCat[CSS_track$selfScore>=12]="4"
 
 
 #Looking at general patterns
@@ -61,33 +90,6 @@ hist(base_data$weight,breaks=40)
 #Regression analyses
 
 
-#Baseline data with self-reports
-
-base_data$selfScore <- (base_data$mobileUseBeforeSleep=="5-7 times per week")*4+(base_data$mobileUseBeforeSleep=="2-4 times per week")*3+(base_data$mobileUseBeforeSleep=="Once a week")*3+(base_data$mobileUseBeforeSleep=="Every month or less")*2+(base_data$mobileUseBeforeSleep=="Never")*1+
-  (base_data$mobileUseNight=="Every night or almost every night")*4+(base_data$mobileUseNight=="A few times a week")*3+(base_data$mobileUseNight=="A few times a month or less")*2+(base_data$mobileUseNight=="Never")*1+
-  (base_data$mobileCheck==">20 times an hour")*4+(base_data$mobileCheck=="11-20 times an hour")*4+(base_data$mobileCheck=="5-10 times an hour")*3+(base_data$mobileCheck=="1-4 times an hour")*2+(base_data$mobileCheck=="Every 2nd hour")*2+(base_data$mobileCheck=="Several times a day")*1+(base_data$mobileCheck=="Once a day or less")*1+
-  (base_data$pmpuScale<=14)*1+(base_data$pmpuScale>14 & base_data$pmpuScale<=16.76)*2+(base_data$pmpuScale>16.76 & base_data$pmpuScale<=19)*3+(base_data$pmpuScale>19)*4
-base_data$selfScoreCat <- "1"
-base_data$selfScoreCat[base_data$selfScore>=8]="2"
-base_data$selfScoreCat[base_data$selfScore>=9.517]="3"
-base_data$selfScoreCat[base_data$selfScore>=12]="4"
-
-base_data$sample_weights <- as.numeric(unique(left_join(base_data[1:(nrow(base_data)/21),],base_weights[,c("id","responseDate","Weights","age","gender","zipCode","riseTime","sleepTime","education","occupation","mobilephone","pmpuScale")],by=c("responseDate","age","gender","zipCode","riseTime","sleepTime","education","occupation","mobilephone","pmpuScale")))$Weights) #use respondDate and variables that are relevant for weight sizes. Then take unique of the data.
-
-#check for matching stuff
-sum(base_data$responseDate[1:(nrow(base_data)/21)]!=unique(left_join(base_data[1:(nrow(base_data)/21),],base_weights[,c("id","responseDate","Weights","age","gender","zipCode","riseTime","sleepTime","education","occupation","mobilephone","pmpuScale")],by=c("responseDate","age","gender","zipCode","riseTime","sleepTime","education","occupation","mobilephone","pmpuScale")))$responseDate)
-sum(base_data$age[1:(nrow(base_data)/21)]!=unique(left_join(base_data[1:(nrow(base_data)/21),],base_weights[,c("id","responseDate","Weights","age","gender","zipCode","riseTime","sleepTime","education","occupation","mobilephone","pmpuScale")],by=c("responseDate","age","gender","zipCode","riseTime","sleepTime","education","occupation","mobilephone","pmpuScale")))$age)
-sum(base_data$gender[1:(nrow(base_data)/21)]!=unique(left_join(base_data[1:(nrow(base_data)/21),],base_weights[,c("id","responseDate","Weights","age","gender","zipCode","riseTime","sleepTime","education","occupation","mobilephone","pmpuScale")],by=c("responseDate","age","gender","zipCode","riseTime","sleepTime","education","occupation","mobilephone","pmpuScale")))$gender)
-sum(base_data$zipCode[1:(nrow(base_data)/21)]!=unique(left_join(base_data[1:(nrow(base_data)/21),],base_weights[,c("id","responseDate","Weights","age","gender","zipCode","riseTime","sleepTime","education","occupation","mobilephone","pmpuScale")],by=c("responseDate","age","gender","zipCode","riseTime","sleepTime","education","occupation","mobilephone","pmpuScale")))$zipCode)
-sum(!(base_weights$Weights%in% base_data$sample_weights))
-#So we have correct weights
-
-base_data$CSSid <- unique(left_join(base_data[1:(nrow(base_data)/21),],base_weights[,c("id","responseDate","Weights","age","gender","zipCode","riseTime","sleepTime","education","occupation","mobilephone","pmpuScale")],by=c("responseDate","age","gender","zipCode","riseTime","sleepTime","education","occupation","mobilephone","pmpuScale")))$id
-
-#We have as many non-na weights as there are rows in the base_weights data. This is a success.
-#All of the id's from base_weights are in use.
-
-
 #Base BMI models
 
 summary(glm((bmi>=30)~(selfScoreCat+age+gender+education+occupation)*sample_weights-sample_weights,data=subset(base_data,imputation!=0),family=binomial))
@@ -113,10 +115,13 @@ summary(new_model)
 
 #Method 1: Using the mice package with mids objects
 base_data_mids <- as.mids(base_data,.imp="imputation")
-summary(glm.mids((bmi>=30)~(selfScoreCat+age+gender+education+occupation)*sample_weights-sample_weights,data=base_data_mids,family=binomial))
-summary(glm.mids((bmi>=25)~(selfScoreCat+age+gender+education+occupation)*sample_weights-sample_weights,data=base_data_mids,family=binomial))
-summay(lm.mids(((bmi^lambda-1)/lambda) ~ (selfScoreCat+age+gender+education+occupation)*sample_weights-sample_weights,data=base_data_mids))
+mod30<-(glm.mids((bmi>=30)~(selfScoreCat+age+gender+education+occupation)*sample_weights-sample_weights,data=base_data_mids,family=binomial))
+mod25<-(glm.mids((bmi>=25)~(selfScoreCat+age+gender+education+occupation)*sample_weights-sample_weights,data=base_data_mids,family=binomial))
+modnum<-(lm.mids(((bmi^lambda-1)/lambda) ~ (selfScoreCat+age+gender+education+occupation)*sample_weights-sample_weights,data=base_data_mids))
 
+summary(pool(mod30))
+summary(pool(mod25))
+summary(pool(modnum))
 
 #Method 2: Estimates adjusted with Rubin's rules (raw computations)
 
@@ -209,14 +214,14 @@ summary(glm(bmi30changeDown ~ (selfScoreCat.y+age.y+gender.y+education.y+occupat
 
 #Method 1: Using the mids object
 
-summary(glm.mids(bmi25change ~ (selfScoreCat.y+age.y+gender.y+education.y+occupation.y)*sample_weights.y-sample_weights.y,data=base_data_mids,family=binomial))
-summary(glm.mids(bmi30change ~ (selfScoreCat.y+age.y+gender.y+education.y+occupation.y)*sample_weights.y-sample_weights.y,data=base_data_mids,family=binomial))
+summary(pool(glm.mids(bmi25change ~ (selfScoreCat.y+age.y+gender.y+education.y+occupation.y)*sample_weights.y-sample_weights.y,data=base_data_mids,family=binomial)))
+summary(pool(glm.mids(bmi30change ~ (selfScoreCat.y+age.y+gender.y+education.y+occupation.y)*sample_weights.y-sample_weights.y,data=base_data_mids,family=binomial)))
 
-summary(glm.mids(bmi25changeUp ~ (selfScoreCat.y+age.y+gender.y+education.y+occupation.y)*sample_weights.y-sample_weights.y,data=base_data_mids,family=binomial))
-summary(glm.mids(bmi30changeUp ~ (selfScoreCat.y+age.y+gender.y+education.y+occupation.y)*sample_weights.y-sample_weights.y,data=base_data_mids,family=binomial))
+summary(pool(glm.mids(bmi25changeUp ~ (selfScoreCat.y+age.y+gender.y+education.y+occupation.y)*sample_weights.y-sample_weights.y,data=base_data_mids,family=binomial)))
+summary(pool(glm.mids(bmi30changeUp ~ (selfScoreCat.y+age.y+gender.y+education.y+occupation.y)*sample_weights.y-sample_weights.y,data=base_data_mids,family=binomial)))
 
-summary(glm.mids(bmi25changeDown ~ (selfScoreCat.y+age.y+gender.y+education.y+occupation.y)*sample_weights.y-sample_weights.y,data=base_data_mids,family=binomial))
-summary(glm.mids(bmi30changeDown ~ (selfScoreCat.y+age.y+gender.y+education.y+occupation.y)*sample_weights.y-sample_weights.y,data=base_data_mids,family=binomial))
+summary(pool(glm.mids(bmi25changeDown ~ (selfScoreCat.y+age.y+gender.y+education.y+occupation.y)*sample_weights.y-sample_weights.y,data=base_data_mids,family=binomial)))
+summary(pool(glm.mids(bmi30changeDown ~ (selfScoreCat.y+age.y+gender.y+education.y+occupation.y)*sample_weights.y-sample_weights.y,data=base_data_mids,family=binomial)))
 
 
 ##Method 2: Rubin's rule for variances - taking into account the imputations - raw computations
@@ -272,26 +277,8 @@ change30down_indicator_table <- data.frame("ests"<-c(coef(glm(bmi30changeDown ~ 
 
 
 
-#Tracking data for the followup CSS sample
+#####Tracking data for the followup CSS sample
 
-#Population sample
-pop_weights <- read.csv2("S:/SUND-IFSV-SmartSleep/Thea/Clusters, obesity and metabolic biomarkers/Data/Population Sample/PopulationWeighted.csv")
-
-
-#Followup sample
-CSS_track <- inner_join(rename(CSS,userid=RespondKey),subject_tracking_clusters,by="userid")
-
-CSS_track$selfScore <- (CSS_track$mobileUseBeforeSleep=="5-7 times per week")*4+(CSS_track$mobileUseBeforeSleep=="2-4 times per week")*3+(CSS_track$mobileUseBeforeSleep=="Once a week")*3+(CSS_track$mobileUseBeforeSleep=="Every month or less")*2+(CSS_track$mobileUseBeforeSleep=="Never")*1+
-  (CSS_track$mobileUseNight=="Every night or almost every night")*4+(CSS_track$mobileUseNight=="A few times a week")*3+(CSS_track$mobileUseNight=="A few times a month or less")*2+(CSS_track$mobileUseNight=="Never")*1+
-  (CSS_track$mobileCheck==">20 times an hour")*4+(CSS_track$mobileCheck=="11-20 times an hour")*4+(CSS_track$mobileCheck=="5-10 times an hour")*3+(CSS_track$mobileCheck=="1-4 times an hour")*2+(CSS_track$mobileCheck=="Every 2nd hour")*2+(CSS_track$mobileCheck=="Several times a day")*1+(CSS_track$mobileCheck=="Once a day or less")*1+
-  (CSS_track$pmpuScale<=14)*1+(CSS_track$pmpuScale>14 & CSS_track$pmpuScale<=16.76)*2+(CSS_track$pmpuScale>16.76 & CSS_track$pmpuScale<=19)*3+(CSS_track$pmpuScale>19)*4
-CSS_track$selfScoreCat <- "1"
-CSS_track$selfScoreCat[CSS_track$selfScore>=8]="2"
-CSS_track$selfScoreCat[CSS_track$selfScore>=9.517]="3"
-CSS_track$selfScoreCat[CSS_track$selfScore>=12]="4"
-
-CSS_weights <- read.csv2("S:/SUND-IFSV-SmartSleep/Thea/Clusters, obesity and metabolic biomarkers/Data/Citizen Science Sample/FollowUpWeighted.csv")
-CSS_track$sample_weights<-as.numeric(left_join(CSS_track,rename(CSS_weights,userid=RespondKey),by="userid")$Weights)
 CSS_track$bmi[CSS_track$bmi==0]<-NA
 
 #Models
@@ -313,7 +300,9 @@ bc <- boxcox(bmi ~ (cluster1prob+cluster2prob+cluster4prob+selfScoreCat+age+gend
 new_model <- lm(((bmi^lambda-1)/lambda) ~ (cluster1prob+cluster2prob+cluster4prob+selfScoreCat+age+gender+education+occupation)*sample_weights-sample_weights,data=subset(CSS_track,impnr!=0))
 
 hist(((CSS_track$bmi^lambda-1)/lambda))
-hist(simulate(new_model),breaks=10)
+hist(simulate(new_model)$sim_1,breaks=20,xlim=c(0.830,0.860))
+plot(fitted(new_model),residuals(new_model))
+
 
 summary(new_model)
 
@@ -322,9 +311,9 @@ summary(new_model)
 
 CSS_track_mids<-as.mids(CSS_track,.imp="impnr",.id="userid")
 
-summary(lm.mids(((bmi^lambda-1)/lambda) ~ (cluster1prob+cluster2prob+cluster4prob+selfScoreCat+age+gender+education+occupation)*sample_weights-sample_weights,data=CSS_track_mids))
-summary(glm.mids((bmi>=25) ~ (cluster1prob+cluster2prob+cluster4prob+selfScoreCat+age+gender+education+occupation)*sample_weights-sample_weights,data=CSS_track_mids,family=binomial))
-summary(glm.mids((bmi>=30) ~ (cluster1prob+cluster2prob+cluster4prob+selfScoreCat+age+gender+education+occupation)*sample_weights-sample_weights,data=CSS_track_mids,family=binomial))
+summary(pool(lm.mids(((bmi^lambda-1)/lambda) ~ (cluster1prob+cluster2prob+cluster4prob+selfScoreCat+age+gender+education+occupation)*sample_weights-sample_weights,data=CSS_track_mids)))
+summary(pool(glm.mids((bmi>=25) ~ (cluster1prob+cluster2prob+cluster4prob+selfScoreCat+age+gender+education+occupation)*sample_weights-sample_weights,data=CSS_track_mids,family=binomial)))
+summary(pool(glm.mids((bmi>=30) ~ (cluster1prob+cluster2prob+cluster4prob+selfScoreCat+age+gender+education+occupation)*sample_weights-sample_weights,data=CSS_track_mids,family=binomial)))
 
 
 #Method 2: Adjusting for multiple imputations in the three kinds of models using Rubin's rules (raw calculations)
