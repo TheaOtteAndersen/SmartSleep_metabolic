f5c0bcb6edd885ee81dc680611d95571b25af6d0
TheaOtteAndersen <than@sund.ku.dk>
2022-05-02 14:55:28 +0200

    update descriptive analysis

--- Analysis_descriptives.R
+++ Analysis_descriptives.R
@@ -20,8 +20,8 @@ library(mice)
 ## Baseline Citizen Science Sample
 setwd("S:/SUND-IFSV-SmartSleep/Data cleaning/Data imputation/Data/Renset imputation/Experiment")
 base_data <- read.table("imp_Experiment.csv", header=TRUE, fill=TRUE, sep=";", stringsAsFactors = TRUE, strip.white = TRUE)
-base_data(base_data$imp_nr)
-base_data <- subset(base_data,imp_nr!=0)
+table(base_data$imputation)
+base_data <- subset(base_data,imputation!=0)
 
 #popu$sample_weights <- as.numeric(unique(left_join(popu[1:(nrow(popu)/21),],base_weights[,c("id","responseDate","Weights","age","gender","zipCode","education","occupation","mobilephone","pmpuScale")],by=c("responseDate","age","gender","zipCode","education","occupation","mobilephone","pmpuScale")))$Weights) #use respondDate and variables that are relevant for weight sizes. Then take unique of the data.
 table(base_data$mobileUseNight, useNA = "always")
@@ -31,22 +31,28 @@ base_data <- subset(base_data, mobilephone!="No mobile phone")
 table(base_data$mobilephone, useNA = "always")
 
 ## self-reported smartphone profiles
-base_data$selfScore <- (base_data$mobileUseBeforeSleep=="5-7 times per week")*4+(base_data$mobileUseBeforeSleep=="2-4 times per week")*3+(base_data$mobileUseBeforeSleep=="Once a week")*3+(base_data$mobileUseBeforeSleep=="Every month or less")*2+(base_data$mobileUseBeforeSleep=="Never")*1+
+publish(univariateTable( ~ mobileUseBeforeSleep,data=base_data, column.percent=TRUE))
+publish(univariateTable( ~ mobileCheck,data=base_data, column.percent=TRUE))
+publish(univariateTable( ~ pmpuScale,data=base_data, column.percent=TRUE))
+summary(base_data$pmpuScale)
+publish(univariateTable( ~ mobileUseNight,data=base_data, column.percent=TRUE))
+
+base_data$selfScore <- ((base_data$mobileUseBeforeSleep=="5-7 times per week")*4+(base_data$mobileUseBeforeSleep=="2-4 times per week")*3+(base_data$mobileUseBeforeSleep=="Once a week")*3+(base_data$mobileUseBeforeSleep=="Every month or less")*2+(base_data$mobileUseBeforeSleep=="Never")*1+
   (base_data$mobileUseNight=="Every night or almost every night")*4+(base_data$mobileUseNight=="A few times a week")*3+(base_data$mobileUseNight=="A few times a month or less")*2+(base_data$mobileUseNight=="Never")*1+
   (base_data$mobileCheck==">20 times per hour")*4+(base_data$mobileCheck=="11-20 times per hour")*4+(base_data$mobileCheck=="5-10 times per hour")*3+(base_data$mobileCheck=="1-4 times per hour")*2+(base_data$mobileCheck=="Every 2nd hour")*2+(base_data$mobileCheck=="Several times per day")*1+(base_data$mobileCheck=="Once a day")*1+
-  (base_data$pmpuScale<14)*1+(base_data$pmpuScale>=14 & base_data$pmpuScale<17)*2+(base_data$pmpuScale>=17 & base_data$pmpuScale<19)*3+(base_data$pmpuScale>=19)*4
+  (base_data$pmpuScale<14)*1+(base_data$pmpuScale>=14 & base_data$pmpuScale<17)*2+(base_data$pmpuScale>=17 & base_data$pmpuScale<19)*3+(base_data$pmpuScale>=19)*4)-4
 summary(base_data$selfScore)
 base_data$selfScore <- as.numeric(base_data$selfScore)
 
 table(base_data$selfScore, useNA="always")
 base_data$selfScoreCat<-NA
 base_data$selfScoreCat[!is.na(base_data$selfScore)] <- "1"
-base_data$selfScoreCat[base_data$selfScore>=8]="2"
-base_data$selfScoreCat[base_data$selfScore>=10]="3"
-base_data$selfScoreCat[base_data$selfScore>=12]="4"
-table(base_data$selfScoreCat,useNA="always")/20 
+base_data$selfScoreCat[base_data$selfScore>=4]="2"
+base_data$selfScoreCat[base_data$selfScore>=6]="3"
+base_data$selfScoreCat[base_data$selfScore>=8]="4"
+table(base_data$selfScoreCat,useNA="always")/25
 base_data$selfScoreCat <- as.factor(base_data$selfScoreCat)
-prop.table(table(base_data$selfScoreCat,useNA="always")/20 )
+prop.table(table(base_data$selfScoreCat,useNA="always")/25 )
 
 ## variables in table 1 (total)
 ## age
@@ -55,48 +61,48 @@ publish(univariateTable( ~ age,data=base_data, column.percent=TRUE))
 
 ## SD for pooled mean age
 
-table(base_data$imp_nr)
-sqrt( mean(aggregate(subset(base_data,imp_nr!=0)$age,by=list(subset(base_data,imp_nr!=0)$imp_nr),FUN=var)$x)+sum((aggregate(subset(base_data,imp_nr!=0)$age,by=list(subset(base_data,imp_nr!=0)$imp_nr),FUN=mean)$x-mean(subset(base_data,imp_nr!=0)$age))^2)/19
+table(base_data$imputation)
+sqrt( mean(aggregate(subset(base_data,imputation!=0)$age,by=list(subset(base_data,imputation!=0)$imputation),FUN=var)$x)+sum((aggregate(subset(base_data,imputation!=0)$age,by=list(subset(base_data,imputation!=0)$imputation),FUN=mean)$x-mean(subset(base_data,imputation!=0)$age))^2)/19
 )
 
 
 ## gender
-table(base_data$gender, useNA="always")/20
-prop.table(table(base_data$gender)/20)
+table(base_data$gender, useNA="always")/25
+prop.table(table(base_data$gender)/25)
 base_data$gender[base_data$gender=="Do not want to answer"] <- "Other"
 
 ## education
-table(base_data$education, useNA="always")/20
+table(base_data$education, useNA="always")/25
 prop.table(table(base_data$education))
 
 ## occupation
-table(base_data$occupation, useNA="always")/20
-prop.table(table(base_data$occupation)/20)
+table(base_data$occupation, useNA="always")/25
+prop.table(table(base_data$occupation)/25)
 
 ## table 1 according to subjective smartphone behavior profiles
-table(base_data$selfScoreCat, useNA="always")/20
-prop.table(table(base_data$selfScoreCat)/20)
+table(base_data$selfScoreCat, useNA="always")/25
+prop.table(table(base_data$selfScoreCat)/25)
 
 ## age ## how to calculate SD for each category??
 publish(univariateTable(selfScoreCat ~ age,data=base_data, column.percent=TRUE))
 
 ## SD for risk profiles 1
 sqrt( 
-  mean(aggregate(subset(base_data,imp_nr!=0 & selfScoreCat==1)$age,by=list(subset(base_data,imp_nr!=0 & selfScoreCat==1)$imp_nr),FUN=var)$x)+sum((aggregate(subset(base_data,imp_nr!=0 & selfScoreCat==1)$age,by=list(subset(base_data,imp_nr!=0 & selfScoreCat==1)$imp_nr),FUN=mean)$x-mean(subset(base_data,imp_nr!=0 & selfScoreCat==1)$age))^2)/19
+  mean(aggregate(subset(base_data,imputation!=0 & selfScoreCat==1)$age,by=list(subset(base_data,imputation!=0 & selfScoreCat==1)$imputation),FUN=var)$x)+sum((aggregate(subset(base_data,imputation!=0 & selfScoreCat==1)$age,by=list(subset(base_data,imputation!=0 & selfScoreCat==1)$imputation),FUN=mean)$x-mean(subset(base_data,imputation!=0 & selfScoreCat==1)$age))^2)/19
 )
 
 ## SD for risk profile 2
 sqrt( 
-  mean(aggregate(subset(base_data,imp_nr!=0 & selfScoreCat==2)$age,by=list(subset(base_data,imp_nr!=0 & selfScoreCat==2)$imp_nr),FUN=var)$x)+sum((aggregate(subset(base_data,imp_nr!=0 & selfScoreCat==2)$age,by=list(subset(base_data,imp_nr!=0 & selfScoreCat==2)$imp_nr),FUN=mean)$x-mean(subset(base_data,imp_nr!=0 & selfScoreCat==2)$age))^2)/19
+  mean(aggregate(subset(base_data,imputation!=0 & selfScoreCat==2)$age,by=list(subset(base_data,imputation!=0 & selfScoreCat==2)$imputation),FUN=var)$x)+sum((aggregate(subset(base_data,imputation!=0 & selfScoreCat==2)$age,by=list(subset(base_data,imputation!=0 & selfScoreCat==2)$imputation),FUN=mean)$x-mean(subset(base_data,imputation!=0 & selfScoreCat==2)$age))^2)/19
 )
 
 ## SD for risk profile 3
 sqrt( 
-  mean(aggregate(subset(base_data,imp_nr!=0 & selfScoreCat==3)$age,by=list(subset(base_data,imp_nr!=0 & selfScoreCat==3)$imp_nr),FUN=var)$x)+sum((aggregate(subset(base_data,imp_nr!=0 & selfScoreCat==3)$age,by=list(subset(base_data,imp_nr!=0 & selfScoreCat==3)$imp_nr),FUN=mean)$x-mean(subset(base_data,imp_nr!=0 & selfScoreCat==3)$age))^2)/19
+  mean(aggregate(subset(base_data,imputation!=0 & selfScoreCat==3)$age,by=list(subset(base_data,imputation!=0 & selfScoreCat==3)$imputation),FUN=var)$x)+sum((aggregate(subset(base_data,imputation!=0 & selfScoreCat==3)$age,by=list(subset(base_data,imputation!=0 & selfScoreCat==3)$imputation),FUN=mean)$x-mean(subset(base_data,imputation!=0 & selfScoreCat==3)$age))^2)/19
 )
 ## SD for risk profile 4
 sqrt( 
-  mean(aggregate(subset(base_data,imp_nr!=0 & selfScoreCat==4)$age,by=list(subset(base_data,imp_nr!=0 & selfScoreCat==4)$imp_nr),FUN=var)$x)+sum((aggregate(subset(base_data,imp_nr!=0 & selfScoreCat==4)$age,by=list(subset(base_data,imp_nr!=0 & selfScoreCat==4)$imp_nr),FUN=mean)$x-mean(subset(base_data,imp_nr!=0 & selfScoreCat==4)$age))^2)/19
+  mean(aggregate(subset(base_data,imputation!=0 & selfScoreCat==4)$age,by=list(subset(base_data,imputation!=0 & selfScoreCat==4)$imputation),FUN=var)$x)+sum((aggregate(subset(base_data,imputation!=0 & selfScoreCat==4)$age,by=list(subset(base_data,imputation!=0 & selfScoreCat==4)$imputation),FUN=mean)$x-mean(subset(base_data,imputation!=0 & selfScoreCat==4)$age))^2)/19
 )
 
 ## gender
@@ -119,8 +125,8 @@ aggregate(base_data$bmi, by=list(base_data$selfScoreCat), FUN=quantile, probs=0.
 setwd("S:/SUND-IFSV-SmartSleep/Data cleaning/Data imputation/Data/Renset imputation/Population Sample")
 popu <- read.table("imp_population.csv", header=TRUE, fill=TRUE, sep=";", stringsAsFactors = TRUE, strip.white = TRUE)
 
-table(popu$imp_nr, useNA = "always")
-popu <- subset(popu,imp_nr!=0)
+table(popu$imputation, useNA = "always")
+popu <- subset(popu,imputation!=0)
 
 ## load tracking data subjects
 setwd("S:/SUND-IFSV-SmartSleep/Data cleaning/Tracking data")
@@ -128,14 +134,14 @@ setwd("S:/SUND-IFSV-SmartSleep/Data cleaning/Tracking data")
 tracking_popu <- subset(subject_tracking_clusters, random==1)
 
 popuAlt <- merge(tracking_popu, popu, by="userid")
-popuAlt <- subset(popuAlt,imp_nr!=0)
+popuAlt <- subset(popuAlt,imputation!=0)
 
 ## distribution in only survey data
 ## age distribution
 publish(univariateTable( ~ agePNR,data=popu, column.percent=TRUE))
 
 ## SD for pooled mean age
-sqrt( mean(aggregate(subset(popu,imp_nr!=0)$agePNR,by=list(subset(popu,imp_nr!=0)$imp_nr),FUN=var)$x)+sum((aggregate(subset(popu,imp_nr!=0)$agePNR,by=list(subset(popu,imp_nr!=0)$imp_nr),FUN=mean)$x-mean(subset(popu,imp_nr!=0)$agePNR))^2)/19
+sqrt( mean(aggregate(subset(popu,imputation!=0)$agePNR,by=list(subset(popu,imputation!=0)$imputation),FUN=var)$x)+sum((aggregate(subset(popu,imputation!=0)$agePNR,by=list(subset(popu,imputation!=0)$imputation),FUN=mean)$x-mean(subset(popu,imputation!=0)$agePNR))^2)/19
 )
 
 ## gender
@@ -250,10 +256,10 @@ prop.table(table(CSS$selfScoreCat,useNA="always"))
 ## sammenligning med den danske population
 
 ## baseline citizen Science sample
-## load baseline data and only keep imp_nr = 0
+## load baseline data and only keep imputation = 0
 setwd("S:/SUND-IFSV-SmartSleep/Data cleaning/Data imputation/Data/Renset imputation/Experiment")
 base <- read.table("imp_Experiment.csv", header=TRUE, fill=TRUE, sep=";", stringsAsFactors = TRUE, strip.white = TRUE)
-base <- subset(base,imp_nr==0)
+base <- subset(base,imputation==0)
 
 ## age groups
 base$ageCat[base$age>=16 & base$age<20] <- "16-19"
@@ -286,8 +292,8 @@ publish(univariateTable( ~ gender,data=base, column.percent=TRUE))
 setwd("S:/SUND-IFSV-SmartSleep/Data cleaning/Data imputation/Data/Renset imputation/Population Sample")
 po <- read.table("imp_population.csv", header=TRUE, fill=TRUE, sep=";", stringsAsFactors = TRUE, strip.white = TRUE)
 
-table(po$imp_nr, useNA = "always")
-po <- subset(po,imp_nr==0)
+table(po$imputation, useNA = "always")
+po <- subset(po,imputation==0)
 
 ## age groups
 po$ageCat[po$agePNR>=18 & po$agePNR<23] <- "18-22"
